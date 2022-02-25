b865cb0b113da18e24debe0f0ec3a82e4ef6d695
Nicolas Medina <nicolas.medina-va@mail.escuelaing.edu.co>
2022-02-24 20:13:32 -0500

    Lab4

---
+++ src/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
+import './Login.css';
+
+export class Login extends React.Component{
+    render(){
+        return (
+
+            <React.Fragment>
+                        <Typography variant="h2">Sign in</Typography>
+                        <form className="form">
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </FormControl>
+                                <Button type="submit" fullWidth variant="contained" color="primary" className="submit">
+                                    Sign in
+                                </Button>
+                        </form>
+            </React.Fragment>
+    );
+    }
+    }
