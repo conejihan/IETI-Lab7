202dba45c99add4286a080a5097fe5ee8ea1de5d
Nicolas Medina <nicolas.medina-va@mail.escuelaing.edu.co>
2022-03-24 18:30:48 -0500

    Lab6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login.js";
-import Tasks from "./Tasks.js";
+import Login from "./Login";
+import Tasks from "./Tasks";
 
 function MainRouter() {
+
     return (
-        <>
+        
             <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/tasks" element={<Tasks />} />
                 </Routes>
             </Router>
-        </>
+        
     );
 }
 
